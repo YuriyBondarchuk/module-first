d89b637aba5b986d6dac41cb3c2d879714fedc0f
YuriyBondarchuk <colortrue1992@gmail.com>
2023-05-29 13:41:51 +0200

    added profile folder

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import './Profile.scss';
+
+function Profile(props: any) {
+    return (
+        <Fragment>
+            work Profile
+        </Fragment>
+    );
+}
+
+export default Profile;
